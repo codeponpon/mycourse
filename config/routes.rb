f23f8b59f7617e74f130d56e126373e6b74e5340
Pond <pond@getmii.com>
2017-06-03 15:04:29 +0700

    Add update user details

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  get 'user_root', to: redirect('/users/edit'), as: :user_root
   root to: "home#index"
 end
