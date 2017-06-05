485ed1e522ae4f5211554509e4be298e4aa65c49
Pond <pond@getmii.com>
2017-06-05 09:00:20 +0700

    Add index/edit/show for course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,23 +1,47 @@
 class CoursesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @courses = Course.paginate(:page => params[:page], :per_page => 30)
   end
 
   def show
+    @course = Course.find_by_id(params[:id])
   end
 
   def new
+    @course = Course.new
   end
 
   def edit
+    @course = Course.find_by_id(params[:id])
   end
 
   def create
+    @course = Course.new(permit_params)
+    if @course.save
+      redirect_to @course
+    else
+      render action: :new
+    end
   end
 
   def update
+    course = Course.find_by_id(params[:id])
+    course.update!(permit_params)
+    redirect_to course
   end
 
   def destroy
+    course = Course.find_by_id(params[:id])
+    redirect_to course_path if course.destroy
+  end
+
+  def search
+  end
+
+  private
+  def permit_params
+    params.require(:course).permit(:name, :description, :category, :subject, :start_time, :end_time)
   end
 end
